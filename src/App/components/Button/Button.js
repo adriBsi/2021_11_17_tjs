1f7059a08bb7f44602f66743003eb00c1d5a551d
adriBsi <adrien.barsotti@gmail.com>
2021-11-17 14:16:22 +0100

    add button component + css

---
+++ src/App/components/Button/Button.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Button.css"
+/**
+ * Composant de button html
+ * @param {object} props props du composant
+ * @returns component render
+ */
+
+const Button = (props) => {
+  return <button className="Button">monButton</button>;
+};
+
+export default Button;
