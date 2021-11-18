6491268efde94789166b258934172db1893064b9
adriBsi <adrien.barsotti@gmail.com>
2021-11-18 12:28:39 +0100

    fix

--- src/App/App.js
+++ src/App/App.js
@@ -1,39 +1,55 @@
 import React from "react";
 import Button from "./components/Button/Button";
 import MemeViewer from "./components/MemeViewer/MemeViewer";
+import FlexLayout from "./components/FlexLayout/FlexLayout";
+import MemeForm from "./components/MemeForm/MemeForm";
+import Header from "./components/Header/Header";
 
 class App extends React.Component {
   counter = 0;
   constructor(props) {
     super(props);
-    this.state={counter:0, value2:0}
+    this.state = { counter: 0, value2: 0 };
   }
-  componentDidUpdate(){
-    console.log('%c%s','font-size:42pt;color:red;background-color:skyblue','le changement est pret et effectif'+this.state.counter);
+  componentDidUpdate() {
+    console.log(
+      "%c%s",
+      "font-size:42pt;color:red;background-color:skyblue",
+      "le changement est pret et effectif" + this.state.counter
+    );
   }
   render() {
     return (
-      <div className="App">
-        <MemeViewer meme={{
-          titre: 'mon 1er meme',
-          text: 'stop la triche',
-          x: 370,
-          y: 530,
-          fontSize: 27,
-          color: 'tomato',
-          fontWeight: '900',
-          underline: true,
-          italic: true,
-          frameX:0,
-          frameY:0,
-        }} image={{
-          id: 0,
-          url: 'img/meme1.jpg',
-          titre: 'meme1',
-          h: 778,
-          w: 736,
-        }} />
-      </div>
+      <>
+        <Header />
+        <div className="App">
+          <FlexLayout>
+            <MemeViewer
+              meme={{
+                titre: "mon 1er meme",
+                text: "stop la triche",
+                x: 370,
+                y: 530,
+                fontSize: 27,
+                color: "tomato",
+                fontWeight: "900",
+                underline: true,
+                italic: true,
+                frameX: 0,
+                frameY: 0,
+              }}
+              image={{
+                id: 0,
+                url: "img/meme1.jpg",
+                titre: "meme1",
+                h: 778,
+                w: 736,
+              }}
+            />
+            <MemeForm />
+          </FlexLayout>
+        </div>
+      </>
     );
   }
 }
--- src/App/components/MemeViewer/MemeViewer.tsx
+++ src/App/components/MemeViewer/MemeViewer.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styles from "./MemeViewer.module.scss";
+import { I_Image, I_Meme } from "../../interfaces/interfaces";
 
-const MemeViewer = (props) => (
+interface I_Props{
+  meme: I_Meme,
+  image?: I_Image
+}
+
+const MemeViewer:React.FC<I_Props> = (props) => {
+  return (
   <svg
     className={styles.MemeViewer}
     data-testid="MemeViewer"
-    viewBox={`0 0 ${props.image.w} ${props.image.h}`}
+    viewBox={`0 0 ${props.image?props.image.w:1000} ${props.image?props.image.h:1000}`}
   >
-    <image href={props.image.url} x="0" y="0" />
+    {props.image && <image href={props.image.url} x="0" y="0" />}
     <text
       x={props.meme.x}
       y={props.meme.y}
@@ -21,12 +28,13 @@ const MemeViewer = (props) => (
     {props.meme.text}
     </text>
   </svg>
-);
-
-MemeViewer.propTypes = {
-  meme: PropTypes.object.isRequired,
-  image: PropTypes.object,
+  )
 };
+
+// MemeViewer.propTypes = {
+//   meme: PropTypes.object.isRequired,
+//   image: PropTypes.object,
+// };
 
 MemeViewer.defaultProps = {};
 
